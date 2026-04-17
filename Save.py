d70816cae2dd73d68ee20005823bee2a4b7254ef
pomboandres46-lab <pomboandres46@gmail.com>
2026-04-17 18:31:51 -0500

    Correcion en la transformacion

--- Save.py
+++ Save.py
@@ -21,6 +21,18 @@ def Guarda(Fuente, data):
                 os.makedirs(os.path.dirname(CSV_FILE_YAHOO))
             
             df_new = pd.DataFrame(data)
+            
+            # Formatear el Dataframe de Yahoo para conservar la fecha y aplanar el MultiIndex
+            df_new.reset_index(inplace=True)
+            if isinstance(df_new.columns, pd.MultiIndex):
+                new_cols = []
+                for col in df_new.columns:
+                    if col[1]:
+                        new_cols.append(f"{col[0]}_{col[1]}")
+                    else:
+                        new_cols.append(col[0])
+                df_new.columns = new_cols
+
             print("Ejecutando Guardado yahoo...")
 
             # Guardado en modo Append
